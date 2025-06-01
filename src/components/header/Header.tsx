705d72bc32ac434acb787ea85c9116edbb0fdaef
vsvir <vladsvir888@gmail.com>
2025-06-01 12:15:35 +0300

    fix: user

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,12 @@
 import ThemeButton from "../theme/ThemeButton";
-import { useUserContext } from "../user/UserContext";
-import UserButton from "../user/UserButton";
-import UsernameDisplay from "../user/UsernameDisplay";
+import UserPanel from "../user/UserPanel";
 import styles from "./header.module.css";
 
 const Header = () => {
-  const { user, login, logout } = useUserContext();
-  const textUserButton = user ? "Выйти" : "Войти";
-  const handleUser = user ? logout : login;
-
   return (
     <header className={styles.header}>
       <ThemeButton />
-      <UserButton text={textUserButton} onClick={handleUser} />
-      {user && <UsernameDisplay />}
+      <UserPanel />
     </header>
   );
 };
